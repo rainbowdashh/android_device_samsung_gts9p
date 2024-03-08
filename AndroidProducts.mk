35ff1a101b7a6c88281e278520119e16567ac524
Alex <simutalexandru@yahoo.com>
2024-03-09 01:13:10 +0200

    This is gts9p nao

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_gts9wifi.mk
+    $(LOCAL_DIR)/twrp_gts9p.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_gts9wifi-user \
-    twrp_gts9wifi-userdebug \
-    twrp_gts9wifi-eng
+    twrp_gts9p-user \
+    twrp_gts9p-userdebug \
+    twrp_gts9p-eng
